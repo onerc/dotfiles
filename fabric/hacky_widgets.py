f15307273740992ac3ad1367939c9f146d46488d
onerc <cngzhnoner@gmail.com>
2026-08-20 22:23:19 +0300

    betterer hiding

--- fabric/hacky_widgets.py
+++ fabric/hacky_widgets.py
@@ -19,14 +19,16 @@ class SlidingWaylandWindow(WaylandWindow):
             **kwargs,
         )
 
+        self.revealer.connect(
+            "notify::child-revealed",
+            lambda *args: (
+                self.hide() if not self.revealer.get_child_revealed() else None
+            ),
+        )
+
     def slide(self):
         self.show()
         self.revealer.set_reveal_child(True)
 
     def unslide(self):
         self.revealer.set_reveal_child(False)
-
-        GLib.timeout_add(
-            100,
-            lambda: (self.hide(), self.revealer.get_child_revealed())[-1],
-        )
